d731418a0275326a49638eb2c987c87a30c98006
Ankit Dewangan <101589446+ankitdew05@users.noreply.github.com>
2022-07-08 21:21:29 +0530

    finalm

--- models/credit.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-const creditSchema = new mongoose.Schema({
-    credit: Number,
-});
-exports.Credit = mongoose.model('Credit', creditSchema);
--- models/shedular.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const sheduleSchema = new mongoose.Schema({
-    id: String,
-    isRunning: Boolean
-});
-exports.Shedular = mongoose.model('Shedular', sheduleSchema);
