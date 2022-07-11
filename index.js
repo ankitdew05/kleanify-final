d7d352440081c44cb83ad3f919d21ab231b41f13
Ankit Dewangan <101589446+ankitdew05@users.noreply.github.com>
2022-07-11 21:06:11 +0530

    x

--- index.js
+++ index.js
@@ -8,6 +8,4 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log("Running on port 5000");
-});
+app.listen(5000);
