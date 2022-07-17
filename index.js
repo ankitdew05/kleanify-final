8e5c1e17aed5c8088eee59c8dbba99bbd4c83641
Ankit Dewangan <101589446+ankitdew05@users.noreply.github.com>
2022-07-18 01:20:17 +0530

    memory node inrease

--- index.js
+++ index.js
@@ -8,4 +8,6 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(5000);
+app.listen(5000,()=>{
+  console.log('Running on Port 5000')
+});
