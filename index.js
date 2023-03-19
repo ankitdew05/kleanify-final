087143ebaf97d6d5db6aac6632f7146566ee972f
Ankit Dewangan <101589446+ankitdew05@users.noreply.github.com>
2023-03-19 23:11:43 +0530

    prerender.io

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+app.use(require('prerender-node').set('prerenderToken', '7Chn38gADzpMesoT5Or7'));
 
 app.use(express.static(path.join(__dirname, 'build')));
 
